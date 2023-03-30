9324d41d7fa124fbf135e398c9bd0b3d455530d7
vetleenb <122876020+vetleenb@users.noreply.github.com>
2023-03-30 13:59:31 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import MovieCard from './Components/MovieCard';
 import SearchhResults  from './Components/SearchhResults';
-//Søkebar fungerte med filtrering før jeg innførte API, som gjør at den nå ikke fungerer som den skal. 
+//Søkebar fungerte med filtrering før jeg innførte API, som gjør at den nå ikke fungerer som den skal. Måtte endre store deler av koden grunnet dette og resultatet ble da ikke optimalt.
 
 
 function App () {
